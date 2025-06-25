262e0c29ec0d9c5dca788d271815dff434b4ba17
jihad805 <bjihad805@gmail.com>
2025-06-26 00:25:29 +0600

    Flower Bank App vrb seleted

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ let balance = 0;
 const balanceDisply = document.getElementById("#balance");
 const amountInput = document.querySelector("#amount");
 const historyList = document.querySelector("#history")
+//vrb selected
