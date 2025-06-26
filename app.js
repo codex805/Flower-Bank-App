ca76b941085d90f099572e5a3729422331fddd72
jihad805 <bjihad805@gmail.com>
2025-06-26 08:36:31 +0600

    Flower Bank App

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const balanceDisply = document.getElementById("#balance");
 const amountInput = document.querySelector("#amount");
 const historyList = document.querySelector("#history")
 //vrb selected
+
+//function diclar 
+function updataBalance(){
+    balanceDisply.innerHTML = `$${balance}`;
+}
+
+function addTransation(type, amount){
+    const li = document.createElement("li");
+}
