fd45f701eef6479f34a524e7f40f6f57f5d5bb65
jihad805 <bjihad805@gmail.com>
2025-06-26 08:47:26 +0600

    Flower Bank App

--- app.js
+++ app.js
@@ -12,4 +12,20 @@ function updataBalance(){
 
 function addTransation(type, amount){
     const li = document.createElement("li");
+    li.className = 'list-group-item';
+    const date = new Date().toLocaleString();
+       li.textContent = `${date} - ${type} - $${amount} (Balance: $${balance})`;
+       historyList.prepend(li);
 }
+
+document.getElementById("#addMoney").addEventListener("click", ()=>{
+    const amout = parseFloat(amountInput.value);
+    if(isNaN(amout) || amout <=0){
+        alert("Please enter a valid positive amount.");
+        return;
+    }
+    balance += amout;
+    updataBalance();
+    addTransation("Add", amout);
+    amountInput.value = '';
+})
