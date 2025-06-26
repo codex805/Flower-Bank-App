c6729529832fa74f9284d4e39b1499c8984581a6
jihad805 <bjihad805@gmail.com>
2025-06-26 09:26:02 +0600

    Bank Applcation All Complete

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
-//js code st
 let balance = 0;
-const balanceDisply = document.querySelector("#balance");
+
+//js code st
+const balanceDisplay = document.querySelector("#balance");
 const amountInput = document.querySelector("#amount");
-const historyList = document.querySelector("#history")
-//vrb selected
+const historyList = document.querySelector("#history");
+
 
 //function diclar 
-function updateBalance(){
-    balanceDisply.innerHTML = `$${balance}`;
+function updateBalance() {
+    balanceDisplay.innerHTML = `$${balance}`;
 }
 
-function addTransaction(type, amount){
+// Add list iteams
+function addTransaction(type, amount) {
     const li = document.createElement("li");
     li.className = 'list-group-item';
     const date = new Date().toLocaleString();
-       li.textContent = `${date} - ${type} - $${amount} (Balance: $${balance})`;
-       historyList.prepend(li);
+    li.textContent = `${date} - ${type} - $${amount} (Balance: $${balance})`;
+    historyList.prepend(li);
 }
 
-document.getElementById("#addMoney").addEventListener("click", ()=>{
+// add money button
+document.querySelector("#addMoney").addEventListener("click", () => {
     const amount = parseFloat(amountInput.value);
-    if(isNaN(amount) || amount <=0){
+    if (isNaN(amount) || amount <= 0) {
         alert("Please enter a valid positive amount.");
         return;
     }
     balance += amount;
     updateBalance();
-    addTransaction()("Add", amount);
+    addTransaction("Add", amount);
     amountInput.value = '';
 });
 
-
-document.querySelector("#withdrawMoney").addEventListener("click", ()=>{
-  const amount = parseFloat(amountInput.value);
-  if(isNaN(amount) || amount <= 0){
-    alert("Please enter a valid positive amount.")
-    return;
-  }
-  if(amount > balance){
-    alert("Insufficient balance.");
-    return;
-  }
-  balance -= amount;
-   updateBalance();
-   addTransaction()("Withdraw", amount);
-   amountInput.value = '';
-})
+// Withdraw Money 
+document.querySelector("#withdrawMoney").addEventListener("click", () => {
+    const amount = parseFloat(amountInput.value);
+    if (isNaN(amount) || amount <= 0) {
+        alert("Please enter a valid positive amount.");
+        return;
+    }
+    if (amount > balance) {
+        alert("Insufficient balance.");
+        return;
+    }
+    balance -= amount;
+    updateBalance();
+    addTransaction("Withdraw", amount);
+    amountInput.value = '';
+});
